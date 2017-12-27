ed7c5545cb027dbbac1536d975705fa182ae79c3
idaylu <idaylu@umich.edu>
2017-12-27 14:56:53 -0600

    First step of adding two numbers of the array.

--- lc-two-sum.rb
+++ lc-two-sum.rb
@@ -2,5 +2,13 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-
+  i = 0
+  while i < nums.length-1
+    nums[i] + nums[i + 1]
+    i += 1
+  end
 end
+
+#Test Case
+nums1 = [1, 2, 5]
+puts two_sum(nums1, 7)
